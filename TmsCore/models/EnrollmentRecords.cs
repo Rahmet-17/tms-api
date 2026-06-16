ab72caf5e1d8af772d987bc429b2fcb13d137ea8
Rahmet_17 <mekaliza49@gmail.com>
2026-06-16 02:35:04 -0700

    :wqRevert "fix: convert TmsCore from submodule to normal folder" :wq This reverts commit f121284873e6fa080ff156dd47534f3c99f32fb3.

--- TmsCore/models/EnrollmentRecords.cs
+++
@@ -1,8 +0,0 @@
-namespace TmsCore.Models;
-
-public record EnrollmentRecord(
-    string Id,
-    string StudentId,
-    string CourseCode,
-    DateTime EnrolledAt
-);
--- TmsCore/models/StudentRecord.cs
+++
@@ -1,7 +0,0 @@
-namespace TmsCore.Models;
-
-public record StudentRecord(
-    string Id,
-    string Name,
-    DateTime CreatedAt,
-    double? Gpa);
